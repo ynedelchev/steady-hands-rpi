a07f7b68a92a7bdab74d33a09e41d9ebf28f378c
Trayan Momkov <33640096+trayanmomkov@users.noreply.github.com>
2017-12-16 15:29:04 +0200

    Increase the pause on touch and add a comment.

--- python/стабилни_ръце.py
+++ python/стабилни_ръце.py
@@ -5,9 +5,11 @@ import RPi.GPIO as GPIO
 import time
 
 # Схема на крачетата: https://www.raspberrypi.org/documentation/usage/gpio/images/a-and-b-physical-pin-numbers.png
+# Забележка: Крачета 3 и 5 винаги отчитат напрежение (1), когато се използват като входни (IN).
 началноКраче = 8
 крайноКраче = 10
 крачеЖица = 12
+секундиПаузаПриНаказание = 0.2
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(началноКраче, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -29,6 +31,7 @@ while True:
                 if GPIO.input(крачеЖица):
                         наказателниТочки = наказателниТочки + 1
                         print("Наказателни точки", наказателниТочки)
-                        time.sleep(0.07)
-        score = time.clock() - началноВреме + (наказателниТочки * 0.07)
+                        time.sleep(секундиПаузаПриНаказание)
+        score = time.clock() - началноВреме + (наказателниТочки * секундиПаузаПриНаказание)
         print("Изминало време", score, "секунди.", наказателниТочки, " наказателни точки")
+        
