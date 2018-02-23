a5a5891fbdfaa01b910e2566b5c78777427b2537
Yordan Nedelchev <yordan.nedelchev@hotmail.com>
2018-02-23 22:06:03 +0000

    Adding a sceleton for Java implementation

---
+++ java/src/main/java/pi/gpio/stradyhands/StradyHandsApplication.java
@@ -0,0 +1,12 @@
+package pi.gpio.stradyhands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StradyHandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StradyHandsApplication.class, args);
+	}
+}
